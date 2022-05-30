a3d209f63dc6b91d841c5aab625651afda3c5216
tuta1612 <lucassaavedra@yahoo.com.ar>
2022-05-30 17:14:38 -0300

    volví a agregar lo de tokbox

--- server.js
+++ server.js
@@ -116,3 +116,41 @@ class Simpson {
                                     oneObject.occupation, oneObject.likes, oneObject.photo, oneObject.other);
     }
   }
+
+
+//TOKBOX
+var apiKey = "46256142";
+var apiSecret = "e34ceb15c0eca4af5bfc5e1b75f374dc913ab744";
+var OpenTok = require('opentok'),
+opentok = new OpenTok(apiKey, apiSecret);
+var sesionDeTokBox = "";
+
+app.post('/videocall-session', function (req, res) {
+    if(sesionDeTokBox.length==0){
+        var sessionOptions = {mediaMode:"routed", archiveMode: "always"};
+        opentok.createSession(sessionOptions, function(err, session){
+            if(err){
+                res.send("no se pudo crear la sesion");
+            }else{
+                sesionDeTokBox = session.sessionId;
+                res.send(sesionDeTokBox);
+            }
+        });
+    } else {
+        res.send(sesionDeTokBox);
+    }
+});
+
+app.delete('/videocall-session', function (req, res) {
+    sesionDeTokBox = "";
+    res.send("se finalizo la sesion de chat");
+});
+
+app.get('/videocall-session', function (req, res) {
+    res.send(sesionDeTokBox);
+});
+
+app.get('/videocall-token', function (req, res) {
+    var token = opentok.generateToken(sesionDeTokBox);
+    res.send(token);
+});
